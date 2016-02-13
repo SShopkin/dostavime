6488dc06b87f040fc1ae12b35e39e87b6391f4f3
SShopkin <monimont2008@abv.bg>
2016-02-13 11:12:03 +0200

    #1 Handle enter button
    Handle enter button if name and pass are correct going to main.html

--- js/script.js
+++ js/script.js
@@ -1,15 +1,48 @@
 $(document).ready(function() {
 	"use strict";
 	
+	function allTrasportLtd() {
+		return $.ajax("http://localhost:3000/transportLtd", {
+			method: "GET",
+			dataType: "json"
+		});
+	}	
+	function allUsers() {
+		return $.ajax("http://localhost:3000/user", {
+			method: "GET",
+			dataType: "json"
+		});
+	}
+	function isItRegistred() {
+		allTrasportLtd().then(function(response) {
+			for(var i=0;i<response.length;i++){
+				if((response[i].username)==($("#indexName").val())){
+					if((response[i].pass)==($("#indexPassword").val())){
+						window.location.href="main.html";
+					}
+				}
+			}
+		});
+		allUsers().then(function(response) {
+			for(var i=0;i<response.length;i++){
+				if((response[i].username)==($("#indexName").val())){
+					if((response[i].pass)==($("#indexPassword").val())){
+						window.location.href="main.html";
+					}
+				}
+			}
+		});
+	}
+	
 	$("#enterButton").click(function(){
-		
+		isItRegistred();
 	});
 	
 	$(".dropdown-menu li").click(function(){
       $("#dropdownHeader").text($(this).text());
       $("#dropdownHeader").val($(this).text());
-
    });
+	
 	
 	
 	
